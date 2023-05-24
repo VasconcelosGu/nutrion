13c95903e7889fffd20534811629d6eb0ec3cb1a
Gustavo Vasconcelos <gustavovasconcelosdepaula@hotmail.com>
2023-05-24 13:07:16 -0300

    adicionando modal de calcculo imc e fechando js das modals, atualizando email de contato e ja fazendo responsivo das modals prontas

--- js/menu.js
+++ js/menu.js
@@ -1,64 +1,49 @@
-const menuButton = document.querySelector('.toggle-menu');
-const menu = document.querySelector('.header-nav');
+const menuButton = document.querySelector(".toggle-menu");
+const menu = document.querySelector(".header-nav");
 
 function toggleMenuIconWhenIsOpen() {
-    const menuIcon = document.querySelector('.toggle-menu i')
-        if (menu.classList.contains('active')){
-            menuIcon.classList.remove('fa-bars')
-            menuIcon.classList.add('fa-times')
-        }else{
-            menuIcon.classList.remove('fa-times')
-            menuIcon.classList.add('fa-bars')
-        }
+  const menuIcon = document.querySelector(".toggle-menu i");
+  if (menu.classList.contains("active")) {
+    menuIcon.classList.remove("fa-bars");
+    menuIcon.classList.add("fa-times");
+  } else {
+    menuIcon.classList.remove("fa-times");
+    menuIcon.classList.add("fa-bars");
+  }
 }
+menuButton.addEventListener("click", () => {
+  menuButton.classList.toggle("active");
+  menu.classList.toggle("active");
 
-function toggleMenu(){
-    menuButton.addEventListener('click', () => {
-        menuButton.classList.toggle('active');
-        menu.classList.toggle('active');   
-        
-        toggleMenuIconWhenIsOpen()
-    })
-}
+  toggleMenuIconWhenIsOpen();
+});
 
+const elementoNav = document.querySelectorAll(".elemento-nav");
+elementoNav.forEach((elemento) =>
+  elemento.addEventListener("click", () => {
+    menuButton.classList.remove("active");
+    menu.classList.remove("active");
+    toggleMenuIconWhenIsOpen();
+  })
+);
 
-function closeWhenMenuElementCliked(){
-    const elementoNav = document.querySelectorAll('.elemento-nav');
+document.addEventListener("click", (event) => {
+  // Verifique se o elemento clicado é o botão do menu ou qualquer elemento dentro do menu
+  const isClickInsideMenu =
+    menu.contains(event.target) ||
+    menuButton === event.target ||
+    menuButton.children[0] === event.target;
 
-elementoNav.forEach((elemento) => (
-    elemento.addEventListener('click' ,() =>{
-        menuButton.classList.remove('active');
-        menu.classList.remove('active');
-        toggleMenuIconWhenIsOpen()
-    })
-))
-}
+  // Se o clique ocorrer fora do menu, o menu deve ser fechado
+  if (!isClickInsideMenu) {
+    menu.classList.remove("active");
+  }
+  toggleMenuIconWhenIsOpen();
+});
 
-
-function closeMenuWhenWindowCliked(){
-    document.addEventListener('click', (event) => {
-        // Verifique se o elemento clicado é o botão do menu ou qualquer elemento dentro do menu
-        const isClickInsideMenu = menu.contains(event.target) || menuButton === event.target || menuButton.children[0] === event.target;
-        
-        // Se o clique ocorrer fora do menu, o menu deve ser fechado
-        if (!isClickInsideMenu) {
-            menu.classList.remove('active');
-        }
-        toggleMenuIconWhenIsOpen()
-    });
-}
-
-
-function closeMenuWhenScroll(){
-    window.addEventListener('scroll', ()=>{
-        if(window.scrollY > 0){
-        menu.classList.remove('active');
-        }
-        toggleMenuIconWhenIsOpen()
-    });
-}
-
-toggleMenu()
-closeWhenMenuElementCliked()
-closeMenuWhenWindowCliked()
-closeMenuWhenScroll()
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    menu.classList.remove("active");
+  }
+  toggleMenuIconWhenIsOpen();
+});
